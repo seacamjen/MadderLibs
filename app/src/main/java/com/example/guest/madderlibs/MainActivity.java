a2b87323b6bdf441fba53bb3fe97d09cb1e8e1db
Cameron Jensen and Gloria Friesen <gloria@>
2017-05-23 16:44:17 -0700

    add radio buttons

--- app/src/main/java/com/example/guest/madderlibs/MainActivity.java
+++ app/src/main/java/com/example/guest/madderlibs/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -19,14 +20,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     EditText mNounPlural1;
     @Bind(R.id.Adjective)
     EditText mAdjective;
-    @Bind(R.id.Verb)
-    EditText mVerb;
+
     @Bind(R.id.NounPlural2)
     EditText mNounPlural2;
     @Bind(R.id.Color)
     EditText mColor;
     @Bind(R.id.BodyPart)
     EditText mBodyPart;
+    String verb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,14 +37,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mSubmitButton.setOnClickListener(this);
     }
-
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+        switch(view.getId()) {
+            case R.id.verbOption1:
+                if(checked)
+                    verb = "smash";
+                break;
+            case R.id.verbOption2:
+                if(checked)
+                    verb = "brush";
+                break;
+        }
+    }
     @Override
     public void onClick(View v) {
         if(v == mSubmitButton) {
             String noun = mNoun.getText().toString();
             String nounPlural1 = mNounPlural1.getText().toString();
             String adjective = mAdjective.getText().toString();
-            String verb = mVerb.getText().toString();
+
             String nounPlural2 = mNounPlural2.getText().toString();
             String color = mColor.getText().toString();
             String bodyPart = mBodyPart.getText().toString();
